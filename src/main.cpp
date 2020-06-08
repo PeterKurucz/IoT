5223c87e84bbfa0437fb07dce885c177ec5d0c0c
Péter Kurucz <peter.kurucz@heitec.hu>
2020-06-08 14:52:02 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,152 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <main.h>
+
+unsigned long previousMillis = 0;
+unsigned long interval = 1000;
+
+unsigned long pumpStateCheckStart = 0;
+unsigned long pump_on_interval = 10000;
+
+enum PUMP_STATE {
+  unknown,
+  switched_on,
+  switched_off
+};
+
+enum PUMP_NOTIFICATION_STATE {
+  undefined,
+  received_on,
+  received_off,
+  in_progress,
+  finished,
+  failed
+};
+
+PUMP_STATE pump_state;
+PUMP_NOTIFICATION_STATE notofication_state;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+  char payload_as_char[length];
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    payload_as_char[i] = (char)payload[i];
+  }
+
+  payload_as_char[length] = '\0';
+
+  if (strcmp(payload_as_char, "on") == 0){
+//    digitalWrite(LED_BUILTIN, LOW);    
+    pump_state = switched_on;
+    notofication_state = received_on;
+    pumpStateCheckStart = millis();
+  } else if (strcmp(payload_as_char, "off") == 0){
+//    digitalWrite(LED_BUILTIN, HIGH);
+    pump_state = switched_off;
+    pumpStateCheckStart = millis();
+  }
+
+  if (strcmp(topic, time_interval) == 0) {
+    pump_on_interval = atol(payload_as_char) * 1000 * 60;
+    Serial.println("interval:");
+    Serial.print(pump_on_interval);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void setup() {
+
+  pump_state = unknown;
+  notofication_state = undefined;
+  pinMode(D0, OUTPUT);
+  digitalWrite(D0, HIGH);
+  
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.subscribe(topic);
+  client.subscribe(time_interval);
+
+  digitalWrite(D0, LOW);
+  delay(250);
+  digitalWrite(D0, HIGH);
+  delay(250);
+  digitalWrite(D0, LOW);
+  delay(250);
+  digitalWrite(D0, HIGH);
+  delay(250);
+  digitalWrite(D0, LOW);
+  delay(250);
+  digitalWrite(D0, HIGH);
+ 
+}
+ 
+void loop() {
+  //  delay(5000);
+  unsigned long currentMillis = millis();
+
+  // check MQTT broker
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    client.loop();
+  }   
+
+  // setup pump
+  if (pump_state == switched_on) {
+        digitalWrite(D0, LOW);
+    if (notofication_state == received_on){
+      client.publish(notification_topic, "received_on");   
+      notofication_state = in_progress;
+      client.publish(notification_topic, "in_progress");
+    }     
+    if (currentMillis - pumpStateCheckStart >= pump_on_interval){
+      pump_state = switched_off;
+      digitalWrite(D0, HIGH);
+    }  
+ } else if (pump_state == switched_off) {
+      digitalWrite(D0, HIGH);
+      if (notofication_state == in_progress){
+        client.publish(notification_topic, "finished");
+        client.publish(topic, "off");
+        notofication_state = finished;
+      }        
+ }
+
+}
